9a3a9a3551efae6c0312be28e488ae9fdd74ced3
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-22 19:01:30 +0300

    created a contact view for the app

--- happy/views.py
+++ happy/views.py
@@ -1,13 +1,8 @@
-from urllib import request
-from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from . models import Contact
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Contact
+from .serializers import ContactSerializer
 
-class ContactList(ListCreateAPIView):
-    def perform_create(self, serializer):
-        serializer.save(owner=request.user)
-
-    def get_queryset(self):
-        return Contact.objects.filter(owner=self.request.user)
-        
+#modelviewset already has mixins for CRUD
+class ContactViewSet(ModelViewSet):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
