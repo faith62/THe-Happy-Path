2285058a58e50911095ebd6f649800f09e39fa8e
faith62 <phifeichemu@gmail.com>
2022-06-22 15:04:40 +0300

    create post request on services

--- happy/views.py
+++ happy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import  Service
 from .serializers import ServiceSerializers
 
@@ -11,4 +12,11 @@ class ServiceList(APIView):
         all_service = Service.objects.all()
         serializers = ServiceSerializers(all_service, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ServiceSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
 # Create your views here.
