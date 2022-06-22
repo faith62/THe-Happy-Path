8f0e3ad2a84e4fdf5af02d56a8cad7a5dd920a1e
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-22 17:38:07 +0300

    add put and delete methods

--- happy/urls.py
+++ happy/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/service/', views.ServiceList.as_view())
+    path('api/service/', views.ServiceList.as_view()),
+    path('api/service/<int:pk>', views.showservice, name='service'),
     
 ]
--- happy/views.py
+++ happy/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import  Service
 from .serializers import ServiceSerializers
+from rest_framework.decorators import api_view
 
 #service 
 class ServiceList(APIView):
@@ -18,5 +19,31 @@ class ServiceList(APIView):
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)   
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def showservice(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        service = Service.objects.get(id=pk)
+    except service.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ServiceSerializers(service)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ServiceSerializers(service, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        service.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+ 
 
