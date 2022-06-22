b6adbb3203ade63dc9a81b1ec1961c6ea2018044
faith62 <phifeichemu@gmail.com>
2022-06-22 14:55:15 +0300

    create an API view to handle the service requests.

--- happy/views.py
+++ happy/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Service
+from .serializers import ServiceSerializers
+
+#........
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_service = Service.objects.all()
+        serializers = ServiceSerializers(all_service, many=True)
+        return Response(serializers.data)
 # Create your views here.
