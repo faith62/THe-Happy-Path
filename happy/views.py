99e6f0fb417f32d8f872b4331e3d34b583c4a704
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-22 17:23:55 +0300

    created a contact view for the app

--- happy/views.py
+++ happy/views.py
@@ -1,3 +1,13 @@
+from urllib import request
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from . models import Contact
 # Create your views here.
+
+class ContactList(ListCreateAPIView):
+    def perform_create(self, serializer):
+        serializer.save(owner=request.user)
+
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
+        
