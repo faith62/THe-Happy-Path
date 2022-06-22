a1f830938e0313d7e4af8a6fda66fc714ad34077
brie <briememo95@gmail.com>
2022-06-22 21:30:22 +0300

    add counsellor models

--- happy/models.py
+++ happy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Counselor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
