4888aff7ae7e953203e487b8f229b55ae9994059
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-22 19:19:19 +0300

    all changes

--- happy/models.py
+++ happy/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.contrib .auth.models import User
 # Create your models here.
+
 class Contact(models.Model):
-    owner = models.ForeignKey(to=User,on_delete=models.CASCADE)
-    firstname= models.CharField(max_length=150)
-    lastname= models.CharField(max_length=150)
+    first_name= models.CharField(max_length=150)
+    last_name= models.CharField(max_length=150)
     email= models.CharField(max_length=150)
     phone_number= models.CharField(max_length=150)
-    loacation= models.CharField(max_length=150)
+    location= models.CharField(max_length=150)
--- happy/urls.py
+++ happy/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path,include
+from . import views 
+from .views import ContactViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router=DefaultRouter()
+router.register('contact',ContactViewSet)
+
 
 urlpatterns = [
-    
+    path('',include(router.urls)),
 ]
