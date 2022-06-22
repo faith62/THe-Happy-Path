e25b1d39565140095155b8c00ecadc3a170b08c6
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-22 23:56:42 +0300

    post and get

--- happy/views.py
+++ happy/views.py
@@ -1,8 +1,25 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Contact
-from .serializers import ContactSerializer
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-#modelviewset already has mixins for CRUD
-class ContactViewSet(ModelViewSet):
-    serializer_class = ContactSerializer
-    queryset = Contact.objects.all()
+from user import serializers
+
+from .serializers import ContactSerializer
+from .models import Contact
+
+
+class contactView(APIView):
+    def get(self,request, *args, **kwargs):
+        queryset = Contact.objects.all()
+        serializer=ContactSerializer(queryset,many=True)
+   
+        return Response(serializer.data)
+
+
+    def post(self,request,*args, **kwargs):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid(): #if all values are correct 
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+        
