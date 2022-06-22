e0603d0c743c945dfc1d64635099d20f8b998b80
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-22 23:57:10 +0300

    modified the models

--- happy/models.py
+++ happy/models.py
@@ -7,3 +7,12 @@ class Contact(models.Model):
     email= models.CharField(max_length=150)
     phone_number= models.CharField(max_length=150)
     location= models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.first_name
+   
+    # def save_contact(self):
+    #     self.save() 
+
+    # def delete_contact(self):
+    #     self.delete()  
