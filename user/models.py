7dbd011db4831023775ce44bacfd3a43ef8fa7a4
rossyvee <roseakinyi001@gmail.com>
2022-06-22 18:25:35 +0300

    creating the class user and updating imports for permission and abstractbaseuser

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from helpers.models import TrackingModel
+from django.contrib.auth.models import PermissionsMixin,BaseUserManager,AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
+    pass
