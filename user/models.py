f715f9c3e8a37ba990de89cec1a715f15c8a238f
rossyvee <roseakinyi001@gmail.com>
2022-06-22 19:53:17 +0300

    creating the class usermanager  and creating the function for user and super user

--- user/models.py
+++ user/models.py
@@ -6,6 +6,33 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 # Create your models here.
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        
+        if not username:
+            raise TypeError('The given username must be set')
+
+        if not email:
+            raise ValueError(('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email,)
+        user.set_password(password)
+        user.save()
+        return user
+       
+
+    def create_superuser(self, username, email, password):
+     
+      if password is None:
+          raise TypeError('Superusers must have a password.')
+
+      user = self.create_user(username, email, password)
+      user.is_superuser = True
+      user.is_staff = True
+      user.save()
+
+      return user
+
 
 class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
     
@@ -27,7 +54,11 @@ class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
     is_active = models.BooleanField(default=True)
 
     date_joined=models.DateTimeField(('date joined'), default=True)
-    
+    email_verified=models.BooleanField(
+        ('email_verified'),
+        default=False,
+        help_text=('Confirm whether this users email is verified')
+    )
     objects =UserManager()
 
     EmailField = 'email'
