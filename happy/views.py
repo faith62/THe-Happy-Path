ac7150ba4847e8da3457564fb69007bfe7918c18
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-23 00:18:58 +0300

    modified the views

--- happy/views.py
+++ happy/views.py
@@ -1,25 +1,52 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-from user import serializers
 
 from .serializers import ContactSerializer
 from .models import Contact
 
 
-class contactView(APIView):
-    def get(self,request, *args, **kwargs):
+class contactList(APIView):
+    def get(self,request, format=None):
         queryset = Contact.objects.all()
         serializer=ContactSerializer(queryset,many=True)
    
         return Response(serializer.data)
 
 
-    def post(self,request,*args, **kwargs):
+    def post(self,request,format=None):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid(): #if all values are correct 
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
-        
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def contact_detail(request, pk):
+    """
+    Retrieve, update or delete a code contact.
+    """
+    try:
+       Contact= Contact.objects.get(pk=pk)
+    except Contact.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer =ContactSerializer(Contact)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ContactSerializer(Contact, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        Contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
