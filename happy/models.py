377c9ccebca86b5c3d57a1379c5dc2d3172593ca
faith62 <phifeichemu@gmail.com>
2022-06-23 13:30:12 +0300

    update serializer

--- happy/models.py
+++ happy/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Contact(models.Model):
+    
     first_name= models.CharField(max_length=150)
     last_name= models.CharField(max_length=150)
     email= models.CharField(max_length=150)
@@ -24,18 +25,11 @@ class Service(models.Model):
     
     category = models.CharField(max_length=50, null=True, choices=CATEGORY)
     description = models.CharField(max_length=800, null=True)    
-    contact = models.ForeignKey(Contact,on_delete=models.SET_NULL,blank=True,null=True)
+    contact = models.ForeignKey(Contact,on_delete=models.SET_NULL,blank=True,null=True,)
 
     def __str__(self):
         return self.category
-    def get_contact_details(self):
-        return{
-            "first_name":self.contact.first_name,
-            "last_name":self.contact.last_name,
-            "email":self.contact.email,
-            "phone_number":self.contact.phone_number,
-            "location":self.contact.location,
-        }
+    
     
     def save_service(self):
         self.save() 
--- happy/serializers.py
+++ happy/serializers.py
@@ -3,13 +3,21 @@ from .models import Contact
 from .models import Service
 
 class ServiceSerializers(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='contact.first_name')
+    contact_last_name = serializers.CharField(source='contact.last_name')
+    contact_phone_number = serializers.CharField(source='contact.phone_number')
+    contact_email = serializers.CharField(source='contact.email')
+    contact_location = serializers.CharField(source='contact.location')
+
     class Meta:
         model = Service
-        fields = ('__all__')
+        fields = '__all__'
+        
 
 class ContactSerializer(serializers.ModelSerializer):
-
+    # service = ServiceSerializers(source='service_set',many=True)
     class Meta:
         model=Contact
         fields='__all__'
+        # fields=('service',)
 
