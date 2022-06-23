021195354bcc8249e0a9772ef404634ebaa6dea4
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-23 13:23:42 +0300

    link contacts to services

--- happy/serializers.py
+++ happy/serializers.py
@@ -3,13 +3,20 @@ from .models import Contact
 from .models import Service
 
 class ServiceSerializers(serializers.ModelSerializer):
+    contact_first_name = serializers.CharField(source='contact.first_name')
+    contact_last_name = serializers.CharField(source='contact.last_name')
+    contact_phone_number = serializers.CharField(source='contact.phone_number')
+    contact_email = serializers.CharField(source='contact.email')
+    contact_location = serializers.CharField(source='contact.location')
     class Meta:
         model = Service
         fields = ('__all__')
 
 class ContactSerializer(serializers.ModelSerializer):
 
+
     class Meta:
         model=Contact
         fields='__all__'
+
 
