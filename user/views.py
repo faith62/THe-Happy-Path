9bb16305ce0b15b279b934534bdb625fbd7bb26c
rossyvee <roseakinyi001@gmail.com>
2022-06-24 08:00:09 +0300

    adding the login class and functions

--- user/views.py
+++ user/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from requests import Response
+from rest_framework.generics import GenericAPIView
+from user import serializers
+from user.serializers import LoginSerializer, RegisterSerializer
+from rest_framework import response, status
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+class RegisterAPIView(GenericAPIView):
+    serializer_class= RegisterSerializer
+     
+    def post(self, request):
+        serializer= self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(GenericAPIView):
+    serializer_class=LoginSerializer
+    
+    def post(self, request):
+        email=request.data.get('email',None)
+        password=request.data.get('password', None)
+
+        user=authenticate(username=email, password=password)
+
+        if user:
+            serializer=self.serializer_class(user)
+            return response.Response(serializer.data, status=status.HTTP_200_OK)
+        return response.Response({'message':"invalid credentials,try again"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
